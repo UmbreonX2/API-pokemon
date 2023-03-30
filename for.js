260f2e19caf099b119a055bfddb74e586bac879a
UmbreonX2 <107277908+UmbreonX2@users.noreply.github.com>
2023-03-30 17:27:08 -0300

    Update for.js

--- for.js
+++ for.js
@@ -20,3 +20,43 @@ async function main(){
 }
 
 main()
+
+const api = require('./api')
+
+async function main(){
+    try {
+        const result = await api.obterPokemon('venusaur')
+        const names = []
+
+
+        // for(let i=0; i<= Object.keys(result.data).length -1; i++){
+        //     const pokemon = result.results[i]
+        //     names.push(pokemon.name) 
+        // }
+
+        // console.log('Nomes:',names)
+
+        console.log(result.data.forms[0].name);
+
+        console.log(Object.keys(result.data).length)
+
+
+        const results = [
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+            "dsjaiodsajiod",
+        ]
+    } catch (error) {
+        console.log('ERROUUU', error)
+    }
+}
+
+main()
